f5e0110a15a7bcbe550ea8d6ea6a309fa868431b
Fabrice PITOISET <fpitpit@users.noreply.github.com>
2023-05-12 19:38:34 +0200

    Build/add application convention (#22)
    * add application convention
    
    * remove duplicate code
    
    * add compose convention

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,86 +1,15 @@
 import fr.pitdev.config.ProjectConfig
 
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    id("jacoco-convention")
+    id("android-application-convention")
+    id("android-application-jacoco-convention")
+    id("android-compose-convention")
 }
 
 android {
     namespace = "com.example.tutojacocokts"
-    compileSdk = ProjectConfig.AndroidConfig.compileSdk
 
     defaultConfig {
         applicationId = "com.example.tutojacocokts"
-        minSdk = ProjectConfig.AndroidConfig.minSdk
-        targetSdk = ProjectConfig.AndroidConfig.targetSdk
-        versionCode = ProjectConfig.AndroidConfig.versionCode
-        versionName = ProjectConfig.AndroidConfig.versionName
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
-
-    buildTypes {
-        debug {
-            enableUnitTestCoverage = true
-
-        }
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = ProjectConfig.AndroidConfig.javaVersion
-        targetCompatibility = ProjectConfig.AndroidConfig.javaVersion
-    }
-    kotlinOptions {
-        jvmTarget = ProjectConfig.AndroidConfig.kotlinJvmVersion
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composeKotlinCompilerExtensionVersion.get()
-    }
-    packaging {
-        resources {
-            excludes += listOf("/META-INF/{AL2.0,LGPL2.1}")
-        }
-    }
-
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-        }
-    }
-
 }
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.bundles.androidx.compose)
-
-    implementation(libs.bundles.androidx.lifecycle)
-
-    testImplementation(libs.junit)
-
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.test.espresso.core)
-
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-}
-
--- build-logic/convention/src/main/kotlin/android-application-jacoco-convention.gradle.kts
+++ build-logic/convention/src/main/kotlin/android-application-jacoco-convention.gradle.kts
@@ -2,6 +2,7 @@ import fr.pitdev.jacoco.configureJacoco
 
 plugins {
     id("jacoco")
+    id("android-application-convention")
 }
 
 project.configureJacoco()
