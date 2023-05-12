9947ec63c4a2b8d9fb0417dff39a137a95ddaf6e
Fabrice PITOISET <fpitoiset.pro@gmail.com>
2023-05-12 19:09:20 +0200

    remove duplicate code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,41 +7,11 @@ plugins {
 
 android {
     namespace = "com.example.tutojacocokts"
-    compileSdk = ProjectConfig.AndroidConfig.compileSdk
 
     defaultConfig {
         applicationId = "com.example.tutojacocokts"
-        minSdk = ProjectConfig.AndroidConfig.minSdk
-        targetSdk = ProjectConfig.AndroidConfig.targetSdk
-        versionCode = ProjectConfig.AndroidConfig.versionCode
-        versionName = ProjectConfig.AndroidConfig.versionName
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
-    buildTypes {
-        debug {
-            enableUnitTestCoverage = true
-
-        }
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = ProjectConfig.AndroidConfig.javaVersion
-        targetCompatibility = ProjectConfig.AndroidConfig.javaVersion
-    }
-    kotlinOptions {
-        jvmTarget = ProjectConfig.AndroidConfig.kotlinJvmVersion
-    }
     buildFeatures {
         compose = true
     }
@@ -53,31 +23,14 @@ android {
             excludes += listOf("/META-INF/{AL2.0,LGPL2.1}")
         }
     }
-
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-        }
-    }
-
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.androidx.compose)
 
-    implementation(libs.bundles.androidx.lifecycle)
-
-    testImplementation(libs.junit)
-    testImplementation(libs.robolectric)
-    testImplementation(libs.jetbrains.kotlinx.coroutines.test)
     testImplementation(libs.androidx.compose.ui.test.junit4)
-
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.test.espresso.core)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
