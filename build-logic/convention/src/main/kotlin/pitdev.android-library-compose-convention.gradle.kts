137fde8a688b4f9e7aaf5287fc7bd074bfa84cb5
Fabrice PITOISET <fabrice.pitoiset.ext@paylib.fr>
2024-03-11 14:36:09 +0100

    build/upgradeAGP : add conventions

---
+++ build-logic/convention/src/main/kotlin/pitdev.android-library-compose-convention.gradle.kts
@@ -0,0 +1,36 @@
+import fr.pitdev.config.libs
+
+private val libs = project.libs
+
+plugins {
+    id("pitdev.android-library-convention")
+}
+
+android {
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.findVersion("composeKotlinCompilerExtensionVersion").get().toString()
+    }
+    packaging {
+        resources {
+            excludes += listOf("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+}
+
+dependencies {
+    implementation(platform(libs.findLibrary("androidx.compose.bom").get()))
+    implementation(libs.findBundle("androidx.compose").get())
+
+    testImplementation(libs.findLibrary("androidx.compose.ui.test.junit4").get())
+
+    androidTestImplementation(platform(libs.findLibrary("androidx.compose.bom").get()))
+    androidTestImplementation(libs.findLibrary("androidx.compose.ui.test.junit4").get())
+
+    debugImplementation(libs.findLibrary("androidx.compose.ui.tooling").get())
+    debugImplementation(libs.findLibrary("androidx.compose.ui.test.manifest").get())
+
+}
