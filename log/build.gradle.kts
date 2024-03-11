b4174e456e4f9f39e148118ff1f9d0260a3c460e
Fabrice PITOISET <fabrice.pitoiset.ext@paylib.fr>
2024-03-11 14:36:09 +0100

    build/upgradeAGP : add conventions

---
+++ log/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("pitdev.android-library-compose-convention")
+}
+
+android {
+    namespace = "fr.pitdev.log"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.android.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
